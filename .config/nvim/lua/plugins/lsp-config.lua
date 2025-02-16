200def063a1193ad99f971c110e03d76e2cd8de0
marshallgjohn <jmarshall@jmpc.>
2025-02-16 12:07:07 -0600

    trying to remove git from directories?

---
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,22 @@
+return {
+  "nvim-lspconfig",
+  opts = {
+    -- ...other opts you might already have...
+    diagnostics = {
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = "",
+          [vim.diagnostic.severity.WARN] = "",
+          [vim.diagnostic.severity.HINT] = "",
+          [vim.diagnostic.severity.INFO] = "",
+        },
+        numhl = {
+          [vim.diagnostic.severity.WARN] = "WarningMsg",
+          [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+          [vim.diagnostic.severity.INFO] = "DiagnosticInfo",
+          [vim.diagnostic.severity.HINT] = "DiagnosticHint",
+        },
+      },
+    },
+  },
+}
